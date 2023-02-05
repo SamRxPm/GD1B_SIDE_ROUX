e9a84db889b7aa3021987dfb15bdbc707de5831c
Sam <118293930+SamRxPm@users.noreply.github.com>
2023-02-05 16:18:56 +0100

    PROG_FINISH

---
+++ jeux_tuiles.tsx
@@ -0,0 +1,89 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Grille_012png" tilewidth="32" tileheight="32" tilecount="160" columns="8">
+ <image source="Grille_012png.png" width="268" height="663"/>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.170122" y="31.8979">
+    <polygon points="0,0 31.6427,-31.8979 31.983,0"/>
+   </object>
+   <object id="2" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0850611" y="0">
+    <polygon points="0,0 32.068,31.8979 0.170122,32.068"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0.0850611">
+    <polygon points="0,0 31.983,31.7278 31.8979,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0850611" y="0">
+    <polygon points="0,0 31.8129,0 0,31.8129"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="45">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0" y="31.8979">
+    <polygon points="0,0 31.8979,-31.8129 32.068,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="46">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0850611" y="0">
+    <polygon points="0,0 31.8979,31.8129 0.0850611,31.8979"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="53">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0850611" y="0.0850611">
+    <polygon points="0,0 31.8979,31.8979 31.8979,-0.0850611"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="54">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0850611" y="0">
+    <polygon points="0,0 31.8129,0 0,31.983"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="85">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="31.8129" y="0.0850611">
+    <polygon points="0,0 -31.8129,31.8129 0,31.8979"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="86">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.170122" y="-0.170122">
+    <polygon points="0,0 32.068,32.2382 0.170122,32.068"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="93">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0850611" y="0">
+    <polygon points="0,0 32.1531,-0.0850611 32.1531,31.983"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="94">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.170122" y="-0.0850611">
+    <polygon points="0,0 32.1531,0 0.255183,31.983"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
